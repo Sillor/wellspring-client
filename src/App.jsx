5c73a03b79d23e5cbf5030d03dd7d4c8f37ea8df
Morningstar515 <tmains515@hotmail.com>
2024-03-26 13:01:09 -0700

    forgot app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
 import './globals.css';
 import {PatientDashboard }from './PatientDashboard/PatientDashboard';
+import { useEffect, useState } from 'react';
+import login from './Login/Login';
+import NewUser from './Login/NewUser'
+
 
 function App() {
-  return (
-      <PatientDashboard/>
-  );
+
+  //Login for development purposes
+  login()
+
+
+  //Call data before routed to dashboard
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    fetch('http://152.44.224.138:5174/patients',{
+    method: 'GET',
+    headers: {
+        'content-type' : 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    },
+},[data])
+.then((res) => res.json())
+.then((data) => {
+    if(data.message === 'success'){
+        localStorage.setItem('token', data.token)
+        setData(data.patients)
+    }
+    else{
+        alert(data.message)
+    }
+})
+
+},[data])
+
+//Dont render if data isnt there
+if(data.length < 1){
+  return(
+    <div>Loading...</div>
+  )
 }
+else{
+  return(<PatientDashboard data={data}/>)}
+
+}
+
+// return(
+//   <>
+//   <NewUser/>
+//   </>
+// )
+
+// }
+
 
 export default App;
