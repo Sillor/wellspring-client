36204e60690bae5b6e94962dc2ba1bdde6304155
Morningstar515 <tmains515@hotmail.com>
2024-05-13 20:10:36 -0700

    fixed login error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import './globals.css';
-import {PatientDashboard }from './PatientDashboard/PatientDashboard';
-import { useEffect, useState } from 'react';
-import login from './Login/Login';
-import NewUser from './Login/NewUser'
 import Dashboard from './ScheduleDashboard/Dashboard';
 
 function App() {
